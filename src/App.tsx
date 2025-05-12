2544787c108f414c0806121d00ff7801997034f3
teaaaaaaaaaaaaaaaaaa <teodora.jovanovac@gmail.com>
2025-05-12 18:06:07 +0200

    prvi commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl bg-pink-700 font-bold text-amber-300">
+      React 19 with Tailwind CSS v4
+    </h1>
+  )
+}
+
+export default App
